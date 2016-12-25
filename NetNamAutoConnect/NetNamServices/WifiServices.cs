ddeae9e4ee20ddbb8f702f5782188776e21fe81e
thanhtdt <thanhtdt@outlook.com>
2016-12-25 21:03:39 +0700

    ss

---
+++ NetNamAutoConnect/NetNamServices/WifiServices.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NetNamServices
+{
+    public static class WifiServices
+    {
+        public static readonly Uri LOGIN_URI = new Uri("http://logoutwifi.netnam.vn/login");
+
+        public static async Task Login(string username, string password)
+        {
+            await Task.Yield();
+            using (HttpClient client = new HttpClient())
+            {
+                FormUrlEncodedContent content = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string,string>("username",username),
+                    new KeyValuePair<string, string>("password",password)
+                });
+                await client.PostAsync(LOGIN_URI, content);
+            }
+        }
+    }
+}
